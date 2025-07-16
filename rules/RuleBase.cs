8e872148b277a650371c6c1054f42e28f7eec38b
Jessewhalen1992 <jesse_whalen@hotmail.com>
2025-07-16 16:25:22 -0300

    feat: add core QA rules and text style check

---
+++ rules/RuleBase.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace CadQa.Rules
+{
+    public abstract class RuleBase
+    {
+        public abstract string Name { get; }
+        public abstract IEnumerable<QaIssue> Evaluate(Database db, Transaction tr);
+    }
+}
