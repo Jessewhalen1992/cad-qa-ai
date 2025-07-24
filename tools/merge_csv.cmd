972e2d796caba0c2ebfbf37175dde90f250f217e
Jessewhalen1992 <jesse_whalen@hotmail.com>
2025-07-24 16:55:51 -0300

    new training files

--- tools/merge_csv.cmd
+++ tools/merge_csv.cmd
@@ -1,6 +1,6 @@
 @echo off
 powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -NoExit ^
-    -File "%~dp0merge_csv.ps1"
+        -File "%~dp0merge_csv.ps1"
 echo.
 echo Press any key to close . . .
 pause >nul
